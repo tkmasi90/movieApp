2c898219600d7eb88f0f581ab638f1e1e61e3b6a
Markus Siitonen <markus.siitonen@tuni.fi>
2024-09-29 16:08:03 +0300

    first implementation, broken

--- MovieNightPlanner/src/main/java/fi/tuni/swdesign/movienightplanner/Movie.java
+++ MovieNightPlanner/src/main/java/fi/tuni/swdesign/movienightplanner/Movie.java
@@ -6,13 +6,15 @@ package fi.tuni.swdesign.movienightplanner;
 
 import java.util.List;
 import com.google.gson.annotations.Expose;
+import java.util.ArrayList;
 
 /**
  *
  * @author janii, Copilot
  */
 public class Movie {
-private boolean adult;
+        @Expose
+        public boolean adult;
         @Expose
         public String backdrop_path;
         @Expose
@@ -39,6 +41,8 @@ private boolean adult;
         public double vote_average;
         @Expose
         public int vote_count;
+        
+        private ArrayList<StreamingProvider> streamingProviders;
 
         public boolean isAdult() {
             return adult;
@@ -150,5 +154,16 @@ private boolean adult;
 
         public void setVoteCount(int vote_count) {
             this.vote_count = vote_count;
+        }
+        
+        public void addStreamingProvider(StreamingProvider sp) {
+            if (this.streamingProviders == null) {
+                this.streamingProviders = new ArrayList<>();
+            }
+            this.streamingProviders.add(sp);
+        }
+        
+        public List<StreamingProvider> getStreamingProviders() {
+            return this.streamingProviders;
         }
     }
--- MovieNightPlanner/src/main/java/fi/tuni/swdesign/movienightplanner/MovieDataController.java
+++ MovieNightPlanner/src/main/java/fi/tuni/swdesign/movienightplanner/MovieDataController.java
@@ -7,9 +7,13 @@ package fi.tuni.swdesign.movienightplanner;
 import java.util.stream.Collectors;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.google.gson.reflect.TypeToken;
 import java.lang.reflect.Type;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.Future;
 import org.apache.hc.client5.http.async.methods.SimpleHttpRequest;
 import org.apache.hc.client5.http.async.methods.SimpleHttpResponse;
@@ -25,7 +29,7 @@ public class MovieDataController {
     
     // List of streaming providers used in the app
     private final List<Integer> PROVIDER_IDS = List.of(337, 8, 119, 76, 323, 338, 2, 350, 2029, 1899);
-    private List<StreamingProvider> streamProviderList;
+    private Map<Integer, StreamingProvider> streamProviderMap;
     
     public PopularMoviesResponse getPopularMovies(){
         
@@ -41,7 +45,7 @@ public class MovieDataController {
 
             // Execute request
             SimpleHttpRequest request1 = SimpleRequestBuilder.get("https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&watch_region=FI").build();
-            request1.addHeader("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjZDc4ZTFkOGE4ZDkyOTc3ODhkZmJlM2U4ZjFmODI2MiIsIm5iZiI6MTcyNjY0NzQxMy4zMTU5MzUsInN1YiI6IjY2ZWE4YjQ0NTE2OGE4OTZlMTFmNDkxZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-Nh_jobP1QsTwiihO2YVhrRTuaX89mle0qVx_nKxZEs");
+            request1.addHeader("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZDAyZjc4ODU0Njc3MTZiYTkyY2Y4YzNjNDY1MTY3OCIsIm5iZiI6MTcyNjczNDgwNS41MzM0MjksInN1YiI6IjY2ZWJlMDgxNjJjNGJiMThjOTc0OGE1NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.cs2Z9pMRYHWmTjnJbeEpLERWzVPHKBsbkJWmwG1Md2o");
             request1.addHeader("accept", "application/json");
 
             Future<SimpleHttpResponse> future = httpclient.execute(request1, null);
@@ -55,7 +59,6 @@ public class MovieDataController {
             .create();
 
             tempMovieList = gson.fromJson(response1.getBodyText(), PopularMoviesResponse.class);
-
         } catch (Exception e){
 
             System.out.println(e);
@@ -64,11 +67,23 @@ public class MovieDataController {
         // TODO:
         // This function doesn't need be called each time.
         // We can fetch this info only on first time and save to a local json
-        if(streamProviderList == null || streamProviderList.isEmpty())
+        if(streamProviderMap == null)
             fetchStreamingProviders();
         else {
             // TODO
         }
+        
+        addStreamingProviders(tempMovieList.getResults());
+        
+        for(var movie : tempMovieList.getResults()) {
+            System.out.println("Title: " + movie.title);
+            System.out.println("Providers:");
+            for(var prov : movie.getStreamingProviders()) {
+                System.out.println(prov.provider_name);
+            }
+            System.out.println();
+        }
+        
         return tempMovieList;
     }  
     
@@ -89,16 +104,74 @@ public class MovieDataController {
             StreamingResponse response = gson.fromJson(response1.getBodyText(), streamingResponseType);
             
             // Filter providers to only have ones that are needed
-            streamProviderList = response.getResults()
+            streamProviderMap = response.getResults()
                     .stream()
                     .filter(p -> PROVIDER_IDS.contains(p.provider_id))
-                    .collect(Collectors.toList());
+                    .collect(Collectors.toMap(StreamingProvider::getProviderId, p -> p));
 
-            for (StreamingProvider provider : streamProviderList) {
+            for (StreamingProvider provider : streamProviderMap.values()) {
                     System.out.println(provider.provider_name + " " + provider.provider_id);
             }
         
         
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+    
+    private void addStreamingProviders(List<Movie> movieList) {
+        for(var movie : movieList) {
+            fetchMovieStreamProviders(movie);
+        }
+    }
+    
+    private void fetchMovieStreamProviders(Movie movie) {
+        SimpleHttpResponse response1;
+        
+        try (CloseableHttpAsyncClient httpclient = HttpAsyncClients.createDefault()) {
+            httpclient.start();
+
+            // Execute request
+            SimpleHttpRequest request1 = SimpleRequestBuilder.get(String.format("https://api.themoviedb.org/3/movie/%s/watch/providers", movie.getId())).build();
+            request1.addHeader("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjZDc4ZTFkOGE4ZDkyOTc3ODhkZmJlM2U4ZjFmODI2MiIsIm5iZiI6MTcyNjY0NzQxMy4zMTU5MzUsInN1YiI6IjY2ZWE4YjQ0NTE2OGE4OTZlMTFmNDkxZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-Nh_jobP1QsTwiihO2YVhrRTuaX89mle0qVx_nKxZEs");
+            request1.addHeader("accept", "application/json");
+
+            Future<SimpleHttpResponse> future = httpclient.execute(request1, null);
+            // and wait until response is received
+            response1 = future.get();
+            System.out.println(request1.getRequestUri() + "->" + response1.getCode());
+
+            Gson gson = new Gson();
+            JsonObject jsonObject = gson.fromJson(response1.getBodyText(), JsonObject.class);
+            JsonObject results = jsonObject.getAsJsonObject("results");
+            if (results.has("FI")) {
+            JsonObject fiData = results.getAsJsonObject("FI");
+            System.out.println("Data for county code FI:");
+
+            // Print the link
+            System.out.println("Link: " + fiData.get("link").getAsString());
+
+            // Retrieve the flatrate array
+            if (fiData.has("flatrate")) {
+                JsonArray flatrateArray = fiData.getAsJsonArray("flatrate");
+
+                // Iterate through the providers and print their details
+                for (JsonElement providerElement : flatrateArray) {
+                    JsonObject providerObj = providerElement.getAsJsonObject();                   
+                    int providerId = providerObj.get("provider_id").getAsInt();
+                    
+                    if(PROVIDER_IDS.contains(providerId)) {
+                        System.out.println("Adding provider: " + streamProviderMap.get(providerId).provider_name);
+                        
+                        movie.addStreamingProvider(streamProviderMap.get(providerId));
+                    }
+                }
+            }
+        } else {
+            System.out.println("No data found for county code FI.");
+        }
+            
+            
         } catch (Exception e) {
             System.out.println(e);
         }
