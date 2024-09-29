296609b2b4d351eb4e2ffd4c6f85d5762336d795
Markus Siitonen <markus.siitonen@tuni.fi>
2024-09-29 11:47:15 +0300

    Created StreamingProvider and response Classes.

--- MovieNightPlanner/src/main/java/fi/tuni/swdesign/movienightplanner/MovieDataController.java
+++ MovieNightPlanner/src/main/java/fi/tuni/swdesign/movienightplanner/MovieDataController.java
@@ -4,8 +4,12 @@
  */
 package fi.tuni.swdesign.movienightplanner;
 
+import java.util.stream.Collectors;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import java.lang.reflect.Type;
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.Future;
 import org.apache.hc.client5.http.async.methods.SimpleHttpRequest;
@@ -19,6 +23,7 @@ import org.apache.hc.client5.http.impl.async.HttpAsyncClients;
  * @author janii
  */
 public class MovieDataController {
+    List<Integer> PROVIDER_IDS = Arrays.asList(337, 8, 119, 76, 323, 338, 2, 350, 2029, 1899);
     
     public PopularMoviesResponse getPopularMovies(){
         
@@ -29,36 +34,67 @@ public class MovieDataController {
         SimpleHttpResponse response1;
         
         try (CloseableHttpAsyncClient httpclient = HttpAsyncClients.createDefault()) {
-        // Start the client
-        httpclient.start();
+            // Start the client
+            httpclient.start();
 
-        // Execute request
-        SimpleHttpRequest request1 = SimpleRequestBuilder.get("https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc").build();
-        request1.addHeader("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjZDc4ZTFkOGE4ZDkyOTc3ODhkZmJlM2U4ZjFmODI2MiIsIm5iZiI6MTcyNjY0NzQxMy4zMTU5MzUsInN1YiI6IjY2ZWE4YjQ0NTE2OGE4OTZlMTFmNDkxZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-Nh_jobP1QsTwiihO2YVhrRTuaX89mle0qVx_nKxZEs");
-        request1.addHeader("accept", "application/json");
-        
-        Future<SimpleHttpResponse> future = httpclient.execute(request1, null);
-        // and wait until response is received
-        response1 = future.get();
-        System.out.println(request1.getRequestUri() + "->" + response1.getCode());
-    
-        Gson gson = new GsonBuilder()
-        .setPrettyPrinting()
-        .excludeFieldsWithoutExposeAnnotation()
-        .create();
-    
-    tempMovieList = gson.fromJson(response1.getBodyText(), PopularMoviesResponse.class);
+            // Execute request
+            SimpleHttpRequest request1 = SimpleRequestBuilder.get("https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&watch_region=FI").build();
+            request1.addHeader("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjZDc4ZTFkOGE4ZDkyOTc3ODhkZmJlM2U4ZjFmODI2MiIsIm5iZiI6MTcyNjY0NzQxMy4zMTU5MzUsInN1YiI6IjY2ZWE4YjQ0NTE2OGE4OTZlMTFmNDkxZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-Nh_jobP1QsTwiihO2YVhrRTuaX89mle0qVx_nKxZEs");
+            request1.addHeader("accept", "application/json");
 
-    } catch (Exception e){
-    
-        System.out.println(e);
-    }
+            Future<SimpleHttpResponse> future = httpclient.execute(request1, null);
+            // and wait until response is received
+            response1 = future.get();
+            System.out.println(request1.getRequestUri() + "->" + response1.getCode());
+
+            Gson gson = new GsonBuilder()
+            .setPrettyPrinting()
+            .excludeFieldsWithoutExposeAnnotation()
+            .create();
+
+            tempMovieList = gson.fromJson(response1.getBodyText(), PopularMoviesResponse.class);
+
+        } catch (Exception e){
+
+            System.out.println(e);
+        }
         
-        
-        
-        
+        getStreamingProvidors();
         
         return tempMovieList;
     }  
     
+    private void getStreamingProvidors() {
+        SimpleHttpResponse response1;
+        StreamingProvider tempStreamProv = null;
+        
+        try (CloseableHttpAsyncClient httpclient = HttpAsyncClients.createDefault()) {
+            httpclient.start();
+            SimpleHttpRequest request1 = SimpleRequestBuilder.get("https://api.themoviedb.org/3/watch/providers/movie?language=en-US&watch_region=FI").build();
+            request1.addHeader("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjZDc4ZTFkOGE4ZDkyOTc3ODhkZmJlM2U4ZjFmODI2MiIsIm5iZiI6MTcyNjY0NzQxMy4zMTU5MzUsInN1YiI6IjY2ZWE4YjQ0NTE2OGE4OTZlMTFmNDkxZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-Nh_jobP1QsTwiihO2YVhrRTuaX89mle0qVx_nKxZEs");
+            request1.addHeader("accept", "application/json");
+            
+            Future<SimpleHttpResponse> future = httpclient.execute(request1, null);
+            // and wait until response is received
+            response1 = future.get();
+            System.out.println(request1.getRequestUri() + "->" + response1.getCode());
+
+            Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
+            Type streamingResponseType = new TypeToken<StreamingResponse>(){}.getType();
+            StreamingResponse response = gson.fromJson(response1.getBodyText(), streamingResponseType);
+            
+            List<StreamingProvider> providers = response.getResults()
+                    .stream()
+                    .filter(p -> PROVIDER_IDS.contains(p.provider_id))
+                    .collect(Collectors.toList());
+            
+                for (StreamingProvider provider : providers) {
+                    System.out.println(provider.provider_name + " " + provider.provider_id);
+            }
+        
+        
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
 }
---
+++ MovieNightPlanner/src/main/java/fi/tuni/swdesign/movienightplanner/StreamingProvider.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fi.tuni.swdesign.movienightplanner;
+
+/**
+ *
+ * @author Make
+ */
+import com.google.gson.annotations.Expose;
+
+public class StreamingProvider {
+
+    @Expose
+    public String provider_name;
+
+    @Expose
+    public int provider_id;
+
+    @Expose
+    public String logo_path;
+
+    // Constructor
+    public StreamingProvider() {}
+
+    // Getters and Setters
+    public String getProviderName() {
+        return provider_name;
+    }
+
+    public void setProviderName(String provider_name) {
+        this.provider_name = provider_name;
+    }
+
+    public int getProviderId() {
+        return provider_id;
+    }
+
+    public void setProviderId(int provider_id) {
+        this.provider_id = provider_id;
+    }
+
+    public String getLogoPath() {
+        return logo_path;
+    }
+
+    public void setLogoPath(String logo_path) {
+        this.logo_path = logo_path;
+    }
+}
+
