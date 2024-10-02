fd9a6ce50acf3b801bf8d1cccda6311cd23acea5
Kian Moloney <kian.moloney@tuni.fi>
2024-10-02 09:41:36 +0300

    Add Top Rated movies beside the popular ones

--- MovieNightPlanner/src/main/java/fi/tuni/swdesign/movienightplanner/MovieDataController.java
+++ MovieNightPlanner/src/main/java/fi/tuni/swdesign/movienightplanner/MovieDataController.java
@@ -4,18 +4,20 @@
  */
 package fi.tuni.swdesign.movienightplanner;
 
-import java.util.stream.Collectors;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
-import java.lang.reflect.Type;
-import java.util.List;
-import java.util.concurrent.Future;
+
 import org.apache.hc.client5.http.async.methods.SimpleHttpRequest;
 import org.apache.hc.client5.http.async.methods.SimpleHttpResponse;
 import org.apache.hc.client5.http.async.methods.SimpleRequestBuilder;
 import org.apache.hc.client5.http.impl.async.CloseableHttpAsyncClient;
 import org.apache.hc.client5.http.impl.async.HttpAsyncClients;
+
+import java.util.stream.Collectors;
+import java.lang.reflect.Type;
+import java.util.List;
+import java.util.concurrent.Future;
 
 /**
  *
@@ -70,7 +72,43 @@ public class MovieDataController {
             // TODO
         }
         return tempMovieList;
-    }  
+    }
+
+    public TopRatedMoviesResponse getTopRatedMovies() {
+        TopRatedMoviesResponse tempMovieList = null;
+
+        // https://hc.apache.org/httpcomponents-client-5.3.x/quickstart.html
+
+        SimpleHttpResponse response1;
+
+        try (CloseableHttpAsyncClient httpclient = HttpAsyncClients.createDefault()) {
+            // Start the client
+            httpclient.start();
+
+            // Execute request
+            SimpleHttpRequest request1 = SimpleRequestBuilder.get("https://api.themoviedb.org/3/movie/top_rated?include_adult=false&language=en-US&page=1watch_region=FI").build();
+            request1.addHeader("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjZDc4ZTFkOGE4ZDkyOTc3ODhkZmJlM2U4ZjFmODI2MiIsIm5iZiI6MTcyNjY0NzQxMy4zMTU5MzUsInN1YiI6IjY2ZWE4YjQ0NTE2OGE4OTZlMTFmNDkxZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-Nh_jobP1QsTwiihO2YVhrRTuaX89mle0qVx_nKxZEs");
+            request1.addHeader("accept", "application/json");
+
+            Future<SimpleHttpResponse> future = httpclient.execute(request1, null);
+            // and wait until response is received
+            response1 = future.get();
+            System.out.println(request1.getRequestUri() + "->" + response1.getCode());
+
+            Gson gson = new GsonBuilder()
+            .setPrettyPrinting()
+            .excludeFieldsWithoutExposeAnnotation()
+            .create();
+
+            tempMovieList = gson.fromJson(response1.getBodyText(), TopRatedMoviesResponse.class);
+
+        } catch (Exception e){
+
+            System.out.println(e);
+        }
+
+        return tempMovieList;
+    }
     
     private void fetchStreamingProviders() {
         SimpleHttpResponse response1;
---
+++ MovieNightPlanner/src/main/java/fi/tuni/swdesign/movienightplanner/TopRatedMoviesResponse.java
@@ -0,0 +1,49 @@
+package fi.tuni.swdesign.movienightplanner;
+
+import java.util.List;
+import com.google.gson.annotations.Expose;
+
+public class TopRatedMoviesResponse {
+    
+    @Expose
+    public int page;
+    @Expose
+    public List<Movie> results;
+    @Expose
+    public int total_pages;
+    @Expose
+    public int total_results;
+
+    // Getters and setters
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public List<Movie> getResults() {
+        return results;
+    }
+
+    public void setResults(List<Movie> results) {
+        this.results = results;
+    }
+
+    public int getTotalPages() {
+        return total_pages;
+    }
+
+    public void setTotalPages(int total_pages) {
+        this.total_pages = total_pages;
+    }
+
+    public int getTotalResults() {
+        return total_results;
+    }
+
+    public void setTotalResults(int total_results) {
+        this.total_results = total_results;
+    }
+}
