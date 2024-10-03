98f560b80598cf81c8d9d9a9d0fa35561d03ab2d
Jani Ilkka <jani.ilkka@tuni.fi>
2024-10-03 17:16:15 +0300

    Adds application state functionality

--- MovieNightPlanner/src/main/java/fi/tuni/swdesign/movienightplanner/App.java
+++ MovieNightPlanner/src/main/java/fi/tuni/swdesign/movienightplanner/App.java
@@ -1,5 +1,7 @@
 package fi.tuni.swdesign.movienightplanner;
 
+import com.google.gson.JsonIOException;
+import java.io.FileNotFoundException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -16,15 +18,31 @@ import javafx.scene.image.Image;
 public class App extends Application {
 
     private static Scene scene;
+    
+    /** AppState object to manage user data and the application state */
+    private AppState appState;
 
     @Override
     public void start(Stage stage) throws IOException {
+        
+        readAppStateFromFile();
+        
         scene = new Scene(loadFXML("primary"));
         stage.setScene(scene);
         stage.setTitle("Movie Night Planner");
         URL logoUrl = this.getClass().getResource("/images/movie_reel.jpeg");
         stage.getIcons().add(new Image(logoUrl.toString()));
         stage.show();
+    }
+    
+    /**
+    * Overrides the default stop method to hijack the application shutdown process.
+    * Saves the current application state to a file before the application 
+    * is stopped completely.
+    */
+    @Override
+    public void stop() {
+        writeAppStateToFile();
     }
 
     static void setRoot(String fxml) throws IOException {
@@ -38,6 +56,33 @@ public class App extends Application {
 
     public static void main(String[] args) {
         launch();
+    }
+    
+    private void readAppStateFromFile() {
+        
+        FileDataController fdc = new FileDataController();
+        
+        try{
+            this.appState = fdc.readStateFromFile();
+        } 
+        catch (FileNotFoundException e){}
+        catch (Exception e){}
+      
+    }
+    
+     /**
+     * Writes the current application state to a file.
+     * 
+     * This method creates a FileDataController object and uses it to write
+     * the current application state to a file.
+     */
+    private void writeAppStateToFile() {
+       FileDataController fdc = new FileDataController();
+        try {
+            fdc.writeStateToFile(this.appState);
+        } catch(JsonIOException | IOException e){
+            System.out.println(e);
+        } 
     }
 
 }
---
+++ MovieNightPlanner/src/main/java/fi/tuni/swdesign/movienightplanner/FileDataController.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fi.tuni.swdesign.movienightplanner;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonIOException;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+
+/**
+ * Provides means to read state data from a file and store state data in a file.
+ * @author janii
+ */
+public class FileDataController {
+    
+    private static final String STATE_FILENAME = "MovieNightPlannerState.json";
+    
+    
+    /**
+    * Reads app state data from a JSON file.
+    * 
+    * @return the AppState object deserialized from the file
+    * @throws FileNotFoundException if the specified file is not found
+    * @throws Exception if an error occurs while reading the file
+    */
+    public AppState readStateFromFile() throws FileNotFoundException, Exception {
+        
+        Gson gson = new GsonBuilder()
+        .setPrettyPrinting()
+        .excludeFieldsWithoutExposeAnnotation()
+        .create();
+        
+        return gson.fromJson(new FileReader(STATE_FILENAME), AppState.class);
+    }
+    
+     /**
+     * Writes state data to a file.
+     * 
+     * @param appState the AppState object to be serialized into a file
+     * @return true if the write operation is successful
+     * @throws com.google.gson.JsonIOException
+     * @throws java.io.IOException
+     */
+    public boolean writeStateToFile(AppState appState) throws JsonIOException, 
+        IOException {
+        
+        Gson gson = new GsonBuilder()
+        .setPrettyPrinting()
+        .excludeFieldsWithoutExposeAnnotation()
+        .create();
+        
+        Writer appStateWriter = new FileWriter(STATE_FILENAME);
+
+        try{
+            gson.toJson(appState, appStateWriter);
+            appStateWriter.flush();
+            appStateWriter.close(); 
+        }catch(JsonIOException | IOException e){
+            
+        }
+        
+        return true;
+    }
+}
